9adfefb665b4ea280b2387a2a9f1168d01380076
Pedro H. N. Vieira <pedrohnv@hotmail.com>
2016-10-21 12:37:38 -0200

    inclusao de rede regressora
    Manipulação extra dos dados para poder treinar uma rede neural regressora; e análise dos resultados.

--- codigosPython/RedeNeuralATP.py
+++ codigosPython/RedeNeuralATP.py
@@ -51,3 +51,67 @@ resultadosErrados.columns = ["previsto/correto"]
 print("Total de acertos:", acertos)
 print("Acertos:", acertos/valoresPadronizados.shape[0]*100, "%")
 resultadosErrados
+
+############ Regressor
+arrayPontos = np.array([])
+for s in arrayKeys: #extrair o ponto da falta
+    arrayPontos = np.append(arrayPontos, float(re.findall('\d+', s)[0]))
+    
+
+#random.seed(234947238) # Reserva 15.44 % para teste; gera um score de ~94 %
+#random.seed(544628) # Reserva 13.56 % para teste; gera um score de ~87 %
+random.seed(444) # Reserva 12.41 % para teste; gera um score de ~66 %
+
+amostrasTreino = np.array([])
+pontosTreino = np.array([])
+amostrasTeste = np.array([])
+pontosTeste = np.array([])
+
+a = 0
+for n in range(arrayPontos.size):
+    if random.gauss(1.05, 1) > 0:
+        try:
+            amostrasTreino = np.vstack((amostrasTreino, valoresPadronizados[n]))
+        except ValueError:
+            amostrasTreino = valoresPadronizados[n]
+
+        pontosTreino = np.append(pontosTreino, arrayPontos[n])
+        
+    else:
+        a += 1
+        try:
+            amostrasTeste = np.vstack((amostrasTeste, valoresPadronizados[n]))
+        except ValueError:
+            amostrasTeste = valoresPadronizados[n]
+
+        pontosTeste = np.append(pontosTeste, arrayPontos[n])
+        
+        
+print("Dados reservados para Teste:", a/arrayPontos.size*100, "%")
+    
+cerebroRegressor = MLPRegressor(activation='logistic', max_iter=5000)
+cerebroRegressor.fit(amostrasTreino, pontosTreino)
+
+score = cerebroRegressor.score(amostrasTeste, pontosTeste)
+print("Acertos:", score, "%")
+
+# residuals teste
+previstos = np.array([])
+corretos = np.array([])
+for n in range(amostrasTeste.shape[0]):
+    previstos = np.append(previstos,
+                          cerebroRegressor.predict(amostrasTeste[n]))
+    
+    corretos = np.append(corretos, pontosTeste[n])
+    
+plt.stem(corretos, previstos-corretos)
+plt.ylabel("Erro (km)")
+plt.xlabel("Ponto correto da falta (km)")
+plt.title("Resíduos dos dados de teste, score: " + str(score))
+plt.show()
+
+print("Média:", np.abs(previstos-corretos).mean())
+print("Desvio Padrão:", np.abs(previstos-corretos).std())
+print("Mediana:", np.median(np.abs(previstos-corretos)))
+print("Máximo:", np.abs(previstos-corretos).max())
+print("Mínimo:", np.abs(previstos-corretos).min())
